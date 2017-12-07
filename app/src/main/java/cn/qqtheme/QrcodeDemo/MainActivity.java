3759d7f2526fb38444794af0ff5b912eddd54ce2
liyujiang <liyujiang_tk@yeah.net>
2017-12-07 10:59:22 +0800

    demo修改

--- app/src/main/java/cn/qqtheme/QrcodeDemo/MainActivity.java
+++ app/src/main/java/cn/qqtheme/QrcodeDemo/MainActivity.java
@@ -1,8 +1,9 @@
 package cn.qqtheme.QrcodeDemo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,13 +25,36 @@ public class MainActivity extends Activity {
         imageView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-                Result result = QrCodeUtils.scanQrCode(bitmap);
-                if (result == null) {
-                    Toast.makeText(MainActivity.this, "二维码识别失败", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_LONG).show();
-                }
+                //final Bitmap bitmap = ((android.graphics.drawable.BitmapDrawable) imageView.getDrawable()).getBitmap();
+                final Bitmap bitmap = toBitmap((View) imageView.getParent());
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setItems(new String[]{"保存二维码", "识别二维码", "取消"}, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if (which == 0) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            ImageView iv = new ImageView(MainActivity.this);
+                            iv.setImageBitmap(bitmap);
+                            builder.setView(iv);
+                            builder.setNeutralButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                            builder.show();
+                        } else if (which == 1) {
+                            Result result = QrCodeUtils.scanQrCode(bitmap);
+                            if (result == null) {
+                                Toast.makeText(MainActivity.this, "二维码识别失败", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+                builder.show();
                 return false;
             }
         });
@@ -52,6 +76,31 @@ public class MainActivity extends Activity {
         } catch (WriterException e) {
             e.printStackTrace();
         }
+    }
+
+    private Bitmap toBitmap(View v) {
+        v.clearFocus();
+        v.setPressed(false);
+        boolean willNotCache = v.willNotCacheDrawing();
+        v.setWillNotCacheDrawing(false);
+        // Reset the drawing cache background color to fully transparent
+        // for the duration of this operation
+        int color = v.getDrawingCacheBackgroundColor();
+        v.setDrawingCacheBackgroundColor(0);
+        if (color != 0) {
+            v.destroyDrawingCache();
+        }
+        v.buildDrawingCache();
+        Bitmap cacheBitmap = v.getDrawingCache();
+        if (cacheBitmap == null) {
+            return null;
+        }
+        Bitmap bitmap = Bitmap.createBitmap(cacheBitmap);
+        // Restore the view
+        v.destroyDrawingCache();
+        v.setWillNotCacheDrawing(willNotCache);
+        v.setDrawingCacheBackgroundColor(color);
+        return bitmap;
     }
 
 }
